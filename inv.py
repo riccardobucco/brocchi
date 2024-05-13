c1cbd1e4623cdcaac7e1f7e73d8fbb9401c25574
Riccardo Bucco <riccardobucco@gmail.com>
2024-05-14 00:39:40 +0200

    Create inv.py

---
+++ inv.py
@@ -0,0 +1,28 @@
+"""
+Function name: inv_
+
+This function computes the inverse of a number, or a list of numbers, or a list of lists of numbers.
+
+Arguments:
+- X: a number, or a list of numbers, or a list of lists of numbers.
+
+Returns:
+- If X is a number, the function returns the inverse of X.
+- If X is a list of numbers, the function returns a list of inverses of the numbers in X.
+- If X is a list of lists of numbers, the function returns a list of lists of inverses of the numbers in X.
+
+Examples:
+inv_(5) returns the inverse of 5 (which is 1/5).
+inv_([1, 2, 3]) returns a list of inverses of 1, 2, and 3.
+inv_([[1, 2], [3, 4]]) returns a list of lists of inverses of 1, 2, 3, and 4.
+"""
+
+def inv_(X):
+    # START CODE HERE
+    # END CODE HERE
+    return value_or_list
+
+
+from test_cases import test_inv_
+
+test_inv_(inv_)

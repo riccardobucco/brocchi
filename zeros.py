09b94b5473783cde038b989903dca3c3be22e5f7
Riccardo Bucco <riccardobucco@gmail.com>
2024-05-14 00:31:58 +0200

    Create zeros.py

---
+++ zeros.py
@@ -0,0 +1,27 @@
+"""
+Function name: zeros_
+
+This function generates a list of zeros with the specified dimensions.
+
+Arguments:
+- *args: a variable number of integers, representing the size of each dimension of the output list.
+
+Returns:
+- If no arguments are provided, the function returns 0.
+- If one argument is provided, the function returns a list of zeros of the specified size.
+
+Examples:
+zeros_() returns 0.0.
+zeros_(5) returns a list of 5 zeros.
+zeros_(2, 3) returns a list of 2 lists, each containing 3 zeros.
+"""
+
+def zeros_(*args):
+    # START CODE HERE
+    # END CODE HERE
+    return value_or_list
+
+
+from test_cases import test_zeros_
+
+test_zeros_(zeros_)

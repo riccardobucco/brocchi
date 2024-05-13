63d4aa432fe3c9965476e7806cb321fdfcff9dad
Riccardo Bucco <riccardobucco@gmail.com>
2024-05-14 00:39:07 +0200

    Create neg.py

---
+++ neg.py
@@ -0,0 +1,28 @@
+"""
+Function name: neg_
+
+This function computes the negation of a number, or a list of numbers, or a list of lists of numbers.
+
+Arguments:
+- X: a number, or a list of numbers, or a list of lists of numbers.
+
+Returns:
+- If X is a number, the function returns the negation of X.
+- If X is a list of numbers, the function returns a list of negations of the numbers in X.
+- If X is a list of lists of numbers, the function returns a list of lists of negations of the numbers in X.
+
+Examples:
+neg_(5) returns the negation of 5 (which is -5).
+neg_([1, 2, 3]) returns a list of negations of 1, 2, and 3.
+neg_([[1, 2], [3, 4]]) returns a list of lists of negations of 1, 2, 3, and 4.
+"""
+
+def neg_(X):
+    # START CODE HERE
+    # END CODE HERE
+    return value_or_list
+
+
+from test_cases import test_neg_
+
+test_neg_(neg_)

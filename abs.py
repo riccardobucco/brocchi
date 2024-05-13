4daffc5d646c862339ead1a16c3a09db901f2f1b
Riccardo Bucco <riccardobucco@gmail.com>
2024-05-14 00:37:49 +0200

    Create abs.py

---
+++ abs.py
@@ -0,0 +1,37 @@
+"""
+Function name: abs_
+
+This function computes the absolute value of a number, or a list of numbers, or a list of lists of numbers.
+
+Absolute value: https://en.wikipedia.org/wiki/Absolute_value
+The absolute value of a number is the non-negative value of the number without considering its sign.
+
+Arguments:
+- X: a number, or a list of numbers, or a list of lists of numbers.
+
+Returns:
+- If X is a number, the function returns the absolute value of X.
+- If X is a list of numbers, the function returns a list of absolute values of the numbers in X.
+- If X is a list of lists of numbers, the function returns a list of lists of absolute values of the numbers in X.
+
+Examples:
+abs_(5) returns the absolute value of 5 (which is 5).
+abs_([-1, 2, -3]) returns a list of absolute values of -1, 2, and -3 (i.e., [1, 2, 3]).
+abs_([[1, -2], [3, -4]]) returns a list of lists of absolute values of 1, -2, 3, and -4 (i.e., [[1, 2], [3, 4]]).
+
+Hint:
+You can use the abs() function to compute the absolute value of a number.
+```
+value = abs(-5)
+```
+"""
+
+def abs_(X):
+    # START CODE HERE
+    # END CODE HERE
+    return value_or_list
+
+
+from test_cases import test_abs_
+
+test_abs_(abs_)

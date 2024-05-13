d0de5a4318d028d878543821d9dcac3ee696d94d
Riccardo Bucco <riccardobucco@gmail.com>
2024-05-14 00:41:44 +0200

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,28 @@
+"""
+Function name: transpose_
+
+This function computes the transpose of a matrix.
+
+Matrix transpose: https://en.wikipedia.org/wiki/Transpose
+The transpose of a matrix is an operator which flips a matrix over its diagonal.
+
+Arguments:
+- X: a list of lists of numbers.
+
+Returns:
+The transpose of the matrix X (a list of lists of numbers).
+
+Examples:
+transpose_([[1, 2], [3, 4], [5, 6]]) returns [[1, 3, 5], [2, 4, 6]].
+transpose_([[1, 2, 3], [4, 5, 6]]) returns [[1, 4], [2, 5], [3, 6]].
+"""
+
+def transpose_(X):
+    # START CODE HERE
+    # END CODE HERE
+    return transposed_matrix
+
+
+from test_cases import test_transpose_
+
+test_transpose_(transpose_)
